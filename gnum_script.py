8e1c660d162c7bad56d45b858e2c3e4a1b24db96
Tenzing J <84193421+TenzingJoseph@users.noreply.github.com>
2022-08-17 12:02:30 +0530

    Numbers script

---
+++ gnum_script.py
@@ -0,0 +1,10 @@
+
+def find_greater():
+  x1 = input('Enter a number: ')
+  x2 = input('Enter another number: ')
+  if x1 > x2:
+    print('The larger number is '+x1)
+  else:
+    print('The larger number is '+x2)
+
+find_greater()
---
+++ snum_script.py
@@ -0,0 +1,10 @@
+
+def find_smaller():
+  x1 = input('Enter a number: ')
+  x2 = input('Enter another number: ')
+  if x1 < x2:
+    print('The smaller number is '+x1)
+  else:
+    print('The smaller number is '+x2)
+
+find_smaller()
